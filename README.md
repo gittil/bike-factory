b99abd9e2287fde7877f993b59210ecaac7027f1
gittil <douglas.littig@gmail.com>
2022-03-31 18:38:52 -0300

    organizando as pastas

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # bike-factory
+
+Detalhar problema de negócio
+
+Explicar qual arquitetura foi escolhida e porque
+
+Fazer passo a passo:
+
+OK 01 - Criar projeto (print)
+
+OK 02- Criar Cloud Storage com a estrutura de pastas (print)
+
+OK 03 - Carregar arquivos da landing area para bucket/RAW
+https://nbviewer.org/github/gittil/bike-factory/blob/main/1-carregando-raw.ipynb
+
+04 - Ler arquivos do bucket/RAW
+
+05 - Criar cluster DATAPROC (print)
+
+06 - Conectar localmente no DATAPROC
+
+07 - Iniciar o tratamento dos arquivos
--- modules/gcp_conector.py
+++ modules/gcp_conector.py
@@ -28,23 +28,23 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
 
 
-# def download_public_file(bucket_name, source_blob_name, destination_file_name):
-#     """Downloads a public blob from the bucket."""
-#     bucket_name = "bike-factory-datalake"
-#     source_blob_name = "bike-factory-datalake/01.RAW"
-#     # destination_file_name = "local/path/to/file"
+def download_public_file(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a public blob from the bucket."""
+    # bucket_name = "bike-factory-datalake"
+    # source_blob_name = "bike-factory-datalake/01.RAW"
+    # destination_file_name = "local/path/to/file"
 
-#     storage_client = storage.Client.create_anonymous_client()
+    storage_client = storage.Client(credentials=credentials)
 
-#     bucket = storage_client.bucket(bucket_name)
-#     blob = bucket.blob(source_blob_name)
-#     blob.download_to_filename(destination_file_name)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
 
-#     print(
-#         "Downloaded public blob {} from bucket {} to {}.".format(
-#             source_blob_name, bucket.name, destination_file_name
-#         )
-#     )
+    print(
+        "Downloaded public blob {} from bucket {} to {}.".format(
+            source_blob_name, bucket.name, destination_file_name
+        )
+    )
 
 
 
